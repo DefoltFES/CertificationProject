e369da8a17a2f5d5f6297ab88862b3131710d931
LordAids <62069378+LordAids@users.noreply.github.com>
2021-05-01 09:50:26 +0300

    Добавил вверстку и логику регистрации
    Не работает только связь с бд, пока пытаюсь настроить.
    Логин пока работает как список пользователей (правда он не работает вообще)

---
+++ Controllers/UserController.cs
@@ -0,0 +1,65 @@
+﻿using CertificationProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CertificationProject.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly TestContext _db;
+        
+        
+        public UserController(TestContext db)
+        {
+           _db = db;
+        }
+
+        public IActionResult Login()
+        {
+            IEnumerable<User> objList = _db.Users;
+            return View(objList);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login(int id, string password)
+        {
+            var obj = _db.Users.Find(id);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            if (obj.Password == password)
+            {
+                return View(obj);
+            }
+            else return RedirectToAction("Registration");
+            
+        }
+
+        public IActionResult Registration()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Registration(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Users.Add(user);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else return View(user);
+
+
+        }
+    }
+}
--- Models/User.cs
+++ Models/User.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
 
 #nullable disable
 
